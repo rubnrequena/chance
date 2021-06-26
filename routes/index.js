c68121d92a9ae2a8e09ea3f37d7fa6f060d8a640
Ruben Requena <rubnrequena@gmail.com>
2021-06-26 15:35:50 +0200

    tomar tiempo del sorteo

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ var axios = require('axios')
 /* GET home page. */
 router.get('/chance', async function (req, res, next) {
   const { hora: time, fecha: today = new Date().toISOString().substr(0, 10) } = req.query;
+  const localTime = getTime(time);
+  console.log(localTime)
   const localDate = today.toString().split("-").reverse().join("/")
   try {
     const data = await axios.get(`http://tuchance.com.ve/Home/FechaResul?fecha=${localDate}`)
     const result = data.data.filter(item => {
-      if (item.fecSorteo.indexOf(` ${time}`) > -1) return true;
+      if (item.fecSorteo.indexOf(` ${localTime}`) > -1) return true;
     })[0]
     if (!result) return res.json({ error: 'no data' })
     res.json({
@@ -24,5 +26,13 @@ router.get('/chance', async function (req, res, next) {
     })
   }
 });
+
+function getTime(/** @type {string} */ time) {
+  const regPattern = /\d{1,2}/;
+  const regResult = regPattern.exec(time);
+  const localTime = regResult[0];
+  console.log(regResult);
+  if (regResult) return `${localTime}:00`;
+}
 
 module.exports = router;
